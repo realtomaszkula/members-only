11fd92fefe40d589eb57ee4a52f96b55e9cf5a4d
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-16 21:33:11 +0100

    Validations for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts
+  validates :name, presence: true, uniqueness: true, length: { in: 3..50 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, length: { in: 3..50 }, format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
+  validates :password, presence: true, length: { in: 3..50 }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do |x|
+  name = Faker::Name.name
+  email = "example#{x}@gmail.com"
+  password = "password"
+  User.create!( name: name,
+                email: email,
+                password: password )
+end
