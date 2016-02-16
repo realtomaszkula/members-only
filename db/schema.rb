2b4543a30b6f616fcefe45b39a550220887c8d67
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-16 18:59:48 +0100

    Add posts controller, model, update datebase

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216173933) do
+ActiveRecord::Schema.define(version: 20160216175551) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "posts", ["title"], name: "index_posts_on_title", unique: true
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name",            null: false
@@ -22,6 +33,6 @@ ActiveRecord::Schema.define(version: 20160216173933) do
     t.datetime "updated_at",      null: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
