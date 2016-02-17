05016809e120c7cbefea3078838ea725a3c07544
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-17 01:10:25 +0100

    Refactor

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  validates :user, presence: true
   validates :title, presence: true, length: { in: 1..100 }
   validates :body, presence: true, length: { in: 1..1000 }
 end
