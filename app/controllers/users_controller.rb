c3b09bdfbdb4ace166969498f861a9f960903d94
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-17 03:25:23 +0100

    Fake user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:sucess] = "User created!"
+      flash[:success] = "User created!"
       redirect_to @user
     else
       render :new
